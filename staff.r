67261fec03bd5dae2395f090a117855255c03adc
fanGam <kosyurakosyura@gmail.com>
2024-10-08 14:06:34 +0500

    update

--- staff.r
+++ staff.r
@@ -30,28 +30,33 @@ mean_score <- mean(scores)
 median_score <- median(scores)
 var_score <- var(scores)
 sd_score <- sd(scores)
-summary(scores)
+# summary_scores <- summary(scores)
 
 # 2. Построить 95% ди для среднего балла, если данные имеют нормальное распределение с дисперсией 16
-conf_int_known_var <- CI(mean_score, sigma=sqrt(16), n=length(scores), conf.level=0.95)
-print(conf_int_known_var)
+conf_int_known_var <- MeanCI(scores, method = "classic", sd=sqrt(16), conf.level=0.95)
+cat("95% доверительный интервал для среднего значения (известная дисперсия):")
+cat("[", conf_int_known_var[1], ", ", conf_int_known_var[2], "]\n")
 
 # 3. Построить 95% ди для среднего балла, если данные имеют нормальное распределение
-conf_int_unknown_var <- t.test(scores, conf.level=0.95)$conf.int
-print(conf_int_unknown_var)
+conf_int_unknown_var <- MeanCI(scores, conf.level=0.95)
+cat("95% доверительный интервал для среднего значения (неизвестная дисперсия):")
+cat("[", conf_int_unknown_var[1], ", ", conf_int_unknown_var[2], ")]n")
 
 # 4. Построить 95% ди для дисперсии балла, если данные имеют нормальное распределение
-conf_int_variance <- VarTest(scores, conf.level=0.95)$conf.int
-print(conf_int_variance)
+conf_int_variance <- VarCI(scores, conf.level=0.95)
+cat("95% доверительный интервал для дисперсии:")
+cat("[", conf_int_variance[1], ", ", conf_int_variance[2], "]\n")
 
 # 5. Построить 95% ди для доли студентов с баллом больше, чем 25
 prop <- sum(scores > 25) / length(scores)
 conf_int_prop <- BinomCI(sum(scores > 25), length(scores), conf.level=0.95)
-print(conf_int_prop)
+cat("95% доверительный интервал для доли студентов с баллом больше 25:")
+cat("[", conf_int_prop[1], ", ", conf_int_prop[2], "]\n")
 
 # Часть 3
 # Считываем данные из файла xls
-dataset <- read_excel("ДанныеТитаник.xls")
+library("readxl")
+dataset <- read_excel("Данные Титаник.xls")
 data <- na.omit(dataset)
 
 # 1 Задание
@@ -64,6 +69,7 @@ conf_interval_var <- VarCI(data$age, conf.level = 0.95, na.rm = TRUE)
 cat('95% доверительный интервал для дисперсии возраста: [', conf_interval_var[1], ',', conf_interval_var[2], ']\n')
 
 # Среднее и 95% доверительный интервал для среднего возраста при известном стандартном отклонении
+age_mean <- mean(data$age)
 conf_interval_known_sd <- age_mean + c(-1, 1) * qnorm(1 - 0.05/2) * (8 / sqrt(sum(!is.na(data$age))))
 cat('95% доверительный интервал для среднего возраста при ср.квадратичном отклонении 8: [', conf_interval_known_sd[1], ',', conf_interval_known_sd[2], ']\n')
 
